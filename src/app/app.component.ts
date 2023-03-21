ddd277abdc5c22c7c49f456be012fe199d0a05d9
Luke <luke.k.rowell@gmail.com>
2023-03-20 23:56:16 -0400

    style updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -225,6 +225,11 @@ export class AppComponent {
     var activity = document.getElementById("chartStack")!;
     var button = document.createElement('button');
     var arrowText = document.createElement('h2');
+    var svg = document.createElement('svg');
+
+    svg.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="20" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+  </svg>`;
 
     button.type = 'button';
     button.innerHTML = buttonText;
@@ -237,7 +242,8 @@ export class AppComponent {
     arrowText.style.marginLeft = "1%";
     arrowText.style.marginRight = "1%";
 
-    activity.append(arrowText);
+    activity.append(svg);
+    //activity.append(arrowText);
     activity.append(button);
 
     /*
